8270f0d6428378e2d58ee3e931aa510c902e2df4
Nina Baculinao <nb2406@columbia.edu>
2015-05-02 17:51:35 -0400

    feat(tile.py): resize tiles to 30 x 30 if they are a different size during init

--- tile.py
+++ tile.py
@@ -2,12 +2,16 @@ import cv2
 import reduction as R
 import similarity as S
 
+TILE_WIDTH = 30
+
 class Tile():
 	def __init__(self, path, title):
 		self.path = path
 		self.title = title
 		self.image = cv2.imread(path, cv2.IMREAD_UNCHANGED)
 		# So that tile image is 30 x 30
+		# if len(self.image[0]) != 30:
+		# 	self.image = R.resize_by_w(self.image, TILE_WIDTH)
 		self.image = R.crop_square(self.image)
 		self.height = len(self.image)
 		self.width = len(self.image[0])
