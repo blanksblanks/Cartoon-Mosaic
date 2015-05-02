92fbb8ae6abf968dc8ebbc4b5cff6f27b30d6f4c
Nina Baculinao <nb2406@columbia.edu>
2015-05-02 11:19:04 -0400

    feat(main.py): python main.py base-image tile-directory generates list of tiles to compose base image

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import sys, os
 import cv2
 import numpy as np
-import reduction as R
-import tile
-import base
+import tile as T
+import base as B
+import similarity as S
 
 """
 1. open target image
@@ -15,49 +16,53 @@ import base
 8. next
 """
 
-def main:
+def entitle(impath, path, format):
+    start = len(path)+1
+    end = len(format)*-1
+    title = impath[start:end]
+    return title
+
+def main():
 	# Check if user has provided a base image and tile library
     if len(sys.argv) < 2:
         sys.exit("Usage: python main.py base-image-path tile-directory-path")
 
+    # Parse command line args
     base_path = sys.argv[1]
     tile_path = sys.argv[2]
     format = ".png"
 
-    base_image = cv2.imread(base_path, cv2.IMREAD_UNCHANGED)
-    # TODO: change this hard coded value
-    R.resize_by_p(base_image, 3000)
-    for :
-    	section = crop(image, )
-    def crop(image, start_y, end_y, start_x, end_x):
-	image = image[start_y:end_y, start_x:end_x]
-	return image
-
-
-	# Parse tile library images first
+	# Read tile library images first
     if os.path.exists(tile_path):
         imfilelist=[os.path.join(tile_path,f) for f in os.listdir(tile_path) if f.endswith(format)]
-        if len(imfilelist) < 1:
+        if len(imfilelist) < 1: # number of tile images
             sys.exit ("Need to specify a path containing .ppm files")
-        NUM_IM = len(imfilelist) # default is 40
-        print "Number of images:", NUM_IM
-        for im in imfilelist:
-            print(el)
-            # Update images and titles list
-            image = cv2.imread(el, cv2.IMREAD_UNCHANGED)
-            start = len(path)+1
-            end = len(format)*-1
-            title = el[start:end]
-            # Generate color histogram
-            # Returned image has its transparent pixels converted to 255,255,255
-            image, chist = color_histogram(image, title)
-            chist_images.append(plot)
-            # chist = color_histogram(image, title)
-            chists.append(chist)
-            images.append(image)
-            titles.append(title)
+        tiles = {} # init dictionary of tile objects
+        for impath in imfilelist:
+            print(impath)
+            imtitle = entitle(impath, tile_path, format)
+            tile = T.Tile(impath, imtitle)
+            tiles[imtitle] = tile
     else:
         sys.exit("The path name does not exist")
+
+    # Next, generate base image
+    base = B.Base(base_path)
+
+    # Find best tiles to compose base image
+    the_chosen = []
+    for histogram in base.histograms:
+        closest = 1.0
+        for key in tiles:
+            tile = tiles[key]
+            distance = S.l1_color_norm(histogram, tiles[key].histogram)
+            if (distance < closest):
+                closest = distance
+                closest_tile = tile
+        the_chosen.append(closest_tile.title)
+
+    print "Okay we're done for now"
+    print the_chosen
 
 
 if __name__ == "__main__": main()
