5f7327385b0e6f9532212b03486a196612098e70
Nina Baculinao <nb2406@columbia.edu>
2015-05-05 12:38:29 -0400

    feat(base, tile): add dominants property to both tile and base class

--- base.py
+++ base.py
@@ -2,7 +2,7 @@ import cv2
 import reduction as R
 import similarity as S
 
-TILE_WIDTH = 15
+TILE_WIDTH = 30
 
 class Base():
 	def __init__(self, path,percent=0):
@@ -12,14 +12,13 @@ class Base():
 			self.image = R.resize_by_p(self.image, percent)
 		self.height = len(self.image)
 		self.width = len(self.image[0])
-	# 	self.histograms = find_quadrants(self) # list of quadrant histograms
-
-	# def find_quadrants(self):
 		self.histograms = []
+		self.dominants = []
 		# Equivalent to for(int j=0; j<self.height-TILE_WIDTH; j+=TILE_WIDTH)
 		# Advantage of this way is if self.width%TILE_WIDTH != 0
 		for j in xrange(0, self.height-TILE_WIDTH, TILE_WIDTH):
-			row = []
+			hist_row = []
+			dom_row = []
 			for i in xrange(0, self.width-TILE_WIDTH, TILE_WIDTH):
 				start_y = j
 				end_y = j + TILE_WIDTH
@@ -27,11 +26,14 @@ class Base():
 				end_x = i + TILE_WIDTH
 				quadrant = R.crop(self.image, start_y, end_y, start_x, end_x)
 				title = "base" + str(end_x) + "-" + str(end_y)
-				histogram, quadrant = S.color_histogram(quadrant, title)
+				histogram, quadrant, colors = S.color_histogram(quadrant, title)
 				# Optional, save histogram as bar graph and return saved path
-				plot_path = S.plot_histogram(histogram, title)
-				row.append(histogram)
-			self.histograms.append(row)
+				plot_path = S.plot_histogram(histogram, title, colors)
+				dominants = S.dominant_color(histogram, colors)
+				hist_row.append(histogram)
+				dom_row.append(dominants)
+			self.histograms.append(hist_row)
+			self.dominants.append(dom_row)
 
 		self.rows = len(self.histograms)
 		self.cols = len(self.histograms[0])
--- tile.py
+++ tile.py
@@ -2,7 +2,7 @@ import cv2
 import reduction as R
 import similarity as S
 
-TILE_WIDTH = 15
+TILE_WIDTH = 30
 
 class Tile():
 	def __init__(self, path, title):
@@ -10,12 +10,13 @@ class Tile():
 		self.title = title
 		self.image = cv2.imread(path, cv2.IMREAD_UNCHANGED)
 		# So that tile image is 30 x 30
-		# if len(self.image[0]) != 30:
+		# if len(self.image[0]) != TILE_WIDTH:
 		# 	self.image = R.resize_by_w(self.image, TILE_WIDTH)
 		self.image = R.crop_square(self.image)
 		self.height = len(self.image)
 		self.width = len(self.image[0])
-		# Image is returned as well because its transparent pixels are adjusted
-		self.histogram, self.image = S.color_histogram(self.image, self.title)
-		plot_path = S.plot_histogram(self.histogram, self.title)
+		self.histogram, self.image, self.colors = S.color_histogram(self.image, self.title)
+		plot_path = S.plot_histogram(self.histogram, self.title, self.colors)
+		self.dominants = S.dominant_colors(self.histogram, self.colors)
+		print self.dominants
 		self.uses = 0
